fea6d94a8b1856480d69b67d4ebceb8b34666d5c
Joey <haskaalo@gmail.com>
2023-05-22 01:41:21 -0400

    Trying threads

--- dbwork/main.py
+++ dbwork/main.py
@@ -34,6 +34,6 @@ if __name__ == "__main__":
             print("\nNO PATH FOUND")
         else:
             print("\n" + path)
-            print("Done in " + str(int(end_time - start_time)) + " seconds!")
+            print("Done in " + "{:.2f}".format(end_time - start_time, 4) + " seconds!")
     else:
         print("Nothing to do")
--- dbwork/shortestpath/bfs.py
+++ dbwork/shortestpath/bfs.py
@@ -1,57 +1,104 @@
-from queue import Queue
+import queue
+import sys
+import threading
+
 import database
+import multiprocessing
+from queue import Queue
+import threading
+import uuid
+
+
+def end(bfs_queue, path_found):
+    while not bfs_queue.empty():
+        bfs_queue.get()
+    path_found.set()
+
+
+# multiprocessing value?
+def bfs(dest: str, bfs_queue: Queue, visited_articles, predecessor, path_found: multiprocessing.Event, lock):
+    db = database.connect()
+
+    while not bfs_queue.empty():
+        if path_found.is_set():
+            end(bfs_queue, path_found)
+            return
+
+        while True:
+            try:
+                if path_found.is_set():
+                    end(bfs_queue, path_found)
+                    return
+
+                article_title = bfs_queue.get(block=False)
+                break
+            except queue.Empty:
+                continue
+
+        adj_articles = db.getAdjacentArticles(article_title)
+
+        for adjacent_article_title in adj_articles:
+            with lock:
+                if adjacent_article_title in visited_articles:
+                    continue
+
+            predecessor[adjacent_article_title] = article_title
+
+            if adjacent_article_title == dest:
+                print("FOUND!")
+                end(bfs_queue, path_found)
+                return
+
+            with lock:
+                visited_articles[adjacent_article_title] = visited_articles[article_title] + 1
+            bfs_queue.put(adjacent_article_title)
 
 
 class BFS:
     def __init__(self, source: str, dest: str):
-        self.db = database.connect()
         self.source = source
         self.dest = dest
-        self.bfs_queue = Queue()
-        self.visited_articles = {}
 
     def printPath(self, dest, predecessor):
         if dest is None:
             return ""
 
         arrow = " -> "
+
         if predecessor[dest] is None:
             arrow = ""
 
         return self.printPath(predecessor[dest], predecessor) + arrow + dest
 
     def compute(self):
-        # STEP 0: Initialisation
-        predecessor = {self.source: None}
-        self.bfs_queue.put(self.source)
-        self.visited_articles[self.source] = 0
+        # STEP 0 : Initialisation
+        bfs_queue = Queue()
+        bfs_queue.put(self.source)
+        predecessor, visited_articles = {self.source: None}, {self.source: 0}
 
-        found = False
-        count = 0
-        while not self.bfs_queue.empty():
-            count += 1
-            article_title = self.bfs_queue.get()
-            print("\rCurrent level: " + str(self.visited_articles[article_title]) +
-                  " and processed: " + str(count)
-                  + " number of articles in queue: " + str(self.bfs_queue.qsize()), end='')
-            adj_articles = self.db.getAdjacentArticles(article_title)
+        path_found = threading.Event()
 
-            for adjacent_article_title in adj_articles:
-                if adjacent_article_title in self.visited_articles:
-                    continue
-                elif adjacent_article_title == self.dest:
-                    predecessor[adjacent_article_title] = article_title
-                    found = True
-                    break
+        num_process = 10
+        lock = threading.RLock()
+        threads = []
 
-                predecessor[adjacent_article_title] = article_title
-                self.bfs_queue.put(adjacent_article_title)
-                self.visited_articles[adjacent_article_title] = self.visited_articles[article_title] + 1
+        #dest: str,
+        # bfs_queue: Queue,
+        # visited_articles,
+        # predecessor,
+        # path_found: multiprocessing.Event,
+        # lock
+        for _ in range(num_process):
+            thread = threading.Thread(target=bfs, args=(self.dest, bfs_queue, visited_articles, predecessor, path_found, lock))
+            thread.start()
+            threads.append(thread)
 
-            if found:
-                break
+        for thread in threads:
+            thread.join()
+            print("a process ended")
 
-        if found:
+        lock.acquire()
+        if path_found.is_set():
             return self.printPath(self.dest, predecessor)
         else:
             return None
